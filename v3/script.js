a467a5528a197768dff7b8fae59c6d1e24bd3547
aevanforeest <aevanforeest@users.noreply.github.com>
2024-11-18 10:43:00 +0100

    Add files via upload

--- v3/script.js
+++ v3/script.js
@@ -13,10 +13,11 @@ function onClickAddPlayer(event) {
 function onClickPlayer(event) {
     var e = event.target;
     // bubble up to .list-item
-    while (!e.classList.contains('list-item') && e) {
+    while (e.classList && !e.classList.contains('list-item')) {
+        console.log(e, e.classList);
         e = e.parentNode;
     }
-    if (e) {
+    if (e.classList) {
         // TODO
         console.log('Edit player: ' + e);
     }
@@ -25,12 +26,13 @@ function onClickPlayer(event) {
 function onTouchEndPlayer(event) {
     var e = event.target;
     // bubble up to .list-item
-    while (!e.classList.contains('list-item') && e) {
+    while (e.classList && !e.classList.contains('list-item')) {
         e = e.parentNode;
     }
-    if (e) {
+    if (e.classList) {
         if (Math.ceil(e.scrollLeft + e.offsetWidth) >= e.scrollWidth) {
             // TODO
+            alert('Delete player');
             console.log('Delete player: ' + e);
         }
     }
@@ -39,10 +41,10 @@ function onTouchEndPlayer(event) {
 function onClickNavigationBar(event) {
     var e = event.target;
     // bubble up to .navigation-item
-    while (!e.classList.contains('navigation-item') && e) {
+    while (e.classList && !e.classList.contains('navigation-item')) {
         e = e.parentNode;
     }
-    if (e) {
+    if (e.classList) {
         document.querySelector('.navigation-item.active').classList.toggle('active');
         e.classList.toggle('active');
         document.querySelector('.page.visible').classList.toggle('visible');
